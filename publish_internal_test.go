d945fc1c00e4fed1d1ef216347238654e9957551
Paulo R. Lima <paulorlima@hyz.is>
2026-09-08 10:47:51 -0300

    go.mod: raise the kyse floor to v0.25.0

--- publish_internal_test.go
+++ publish_internal_test.go
@@ -745,7 +745,7 @@ func authFile(t *testing.T, name string) string {
 // they drift apart.
 const (
 	publishedFramework = "v0.46.0"
-	publishedKyse      = "v0.24.0"
+	publishedKyse      = "v0.25.0"
 	publishedHesape    = "v0.33.0"
 )
 
